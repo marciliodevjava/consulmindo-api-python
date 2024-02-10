d745affe15c1193488def89c34e934a5e669b03b
marciliodevjava <marciliodevjava@gmail.com>
2024-02-09 22:57:28 -0300

    Update nomes_hoteis.py

---
+++ diretorio/nomes_hoteis.py
@@ -0,0 +1,17 @@
+NOMES_ID_sites = [
+    {
+        'site_id': 'omega'
+    },
+    {
+        'site_id': 'zulu'
+    },
+    {
+        'site_id': 'november'
+    },
+    {
+        'site_id': 'golf'
+    },
+    {
+        'site_id': ''
+    }
+]

46b4e2575a264f9b5bf8133a18f0a64237acfb21
marciliodevjava <marciliodevjava@gmail.com>
2024-02-12 14:32:38 -0300

    Create nomes_sites.py

---
+++ diretorio/nomes_sites.py
@@ -0,0 +1,42 @@
+NOMES_SITES = [
+    {
+        "url": "www.daily.com.br",
+        "nome": "Daily SA"
+    },
+    {
+        "url": "www.execucao.com.br",
+        "nome": "Execucao SA"
+    },
+    {
+        "url": "www.data.com.br",
+        "nome": "Data SA"
+    },
+    {
+        "url": "www.teste.com.br",
+        "nome": "Teste SA"
+    },
+    {
+        "url": "www.finix.com.br",
+        "nome": "Finix SA"
+    },
+    {
+        "url": "www.erro.com.br",
+        "nome": "Erro SA"
+    },
+    {
+        "url": "www.cadeira.com.br",
+        "nome": "Cadeira SA"
+    },
+    {
+        "url": "www.vaso.com.br",
+        "nome": "vaso SA"
+    },
+    {
+        "url": "www.predendor.com.br",
+        "nome": "Prendedor SA"
+    },
+    {
+        "url": "www.sofa.com.br",
+        "nome": "Sofa SA"
+    }
+]

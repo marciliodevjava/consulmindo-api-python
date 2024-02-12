a08c9cf50f7c39d2bf9e45fcc137f30af20996cb
marciliodevjava <marciliodevjava@gmail.com>
2024-02-12 14:36:03 -0300

    Create url_base

---
+++ diretorio/url_base.py
@@ -0,0 +1,18 @@
+# URL
+URL = 'http://127.0.0.1'
+PORT = ':5050'
+HOTEIS = '/hoteis'
+HOTEL = '/hotel'
+LOGIN = '/login'
+SITES = '/sites'
+SITE = '/site'
+
+# URL montadas
+URL_BASE = URL + PORT
+
+# URL concatenadas
+URL_HOTEIS = URL_BASE + HOTEIS
+URL_LOGIN = URL_BASE + LOGIN
+URL_SITES = URL_BASE + SITES
+URL_HOTEL = URL_BASE + HOTEL
+URL_SITE = URL_BASE + SITE

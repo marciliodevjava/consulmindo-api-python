942cb4aaadda2ceb004b3200ce3e91ce280dd716
marciliodevjava <marciliodevjava@gmail.com>
2024-02-12 14:32:16 -0300

    Update main

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import requests
 
 from diretorio import nomes_hoteis
+from diretorio.url_base import URL_GET_LOGIN, URL_GET_HOTEIS, URL_SITES
 
-URL_BASE = 'http://127.0.0.1:5000'
-HOTEIS = '/hoteis'
-LOGIN = '/login'
-
-URL_GET_HOTEIS = URL_BASE + HOTEIS
-URL_GET_LOGIN = URL_BASE + LOGIN
-LIST_ID_SITES = nomes_hoteis.NOMES_ID_sites
+LIST_ID_SITES = nomes_hoteis.NOMES_ID_HOTEIS
 
 resposta = None
 BEARER = 'Bearer '
@@ -35,5 +30,17 @@ resposta_hoteis = requests.request('GET', URL_GET_HOTEIS, headers=headers)
 if resposta_hoteis.status_code == 200:
     hoteis = resposta_hoteis.json()
     hotel = hoteis['Hoteis']
+    print('Imprimendo Hoteis Cadastrados')
     for h in hotel:
         print(dict(h))
+
+print('=======================================================================')
+
+resposta_sites = requests.request('GET', URL_SITES)
+
+if resposta_sites.status_code == 200:
+    sites = resposta_sites.json()
+    site = sites['Sites']
+    print('Imprimento Sites Cadastrados')
+    for s in site:
+        print(dict(s))

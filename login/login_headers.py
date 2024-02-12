b5d4263c0cc61cb74afa362bd7aadcee379dfb93
marciliodevjava <marciliodevjava@gmail.com>
2024-02-12 18:20:13 -0300

    Update login_headers.py

--- login/login_headers.py
+++ login/login_headers.py
@@ -10,12 +10,18 @@ login = {
     "senha": "123"
 }
 
+print('Efetuando login na aplicação')
 resposta_login = requests.request('POST', URL_LOGIN, json=login)
 
 if resposta_login.status_code == 200:
+    print('Login efetuado com sucesso"')
+    print("=======================================================================")
     resposta = resposta_login.json()
     access_token = resposta.get('acces-token')
     TOKEN = BEARER + access_token
+else:
+    print('Login Falhou.')
+    print("=======================================================================")
 
 headers = {
     'Authorization': TOKEN

b3efce8e79ce424f2bbb7c52194557ef453e4ff6
marciliodevjava <marciliodevjava@gmail.com>
2024-02-13 15:19:57 -0300

    Create dados_hoteis.py

---
+++ diretorio/dados_hoteis.py
@@ -0,0 +1,72 @@
+DADOS_HOTEIS = [
+    {
+        "nome": "Omega Hotel",
+        "estrelas": 3.2,
+        "diaria": 250.90,
+        "cidade": "Rio de Janeiro",
+        "site_id": 1
+    },
+    {
+        "nome": "Zulu Hotel",
+        "estrelas": 2.2,
+        "diaria": 150.90,
+        "cidade": "Cuiaba",
+        "site_id": 2
+    },
+    {
+        "nome": "November Hotel",
+        "estrelas": 3.6,
+        "diaria": 266.90,
+        "cidade": "Porto Alegre",
+        "site_id": 3
+    },
+    {
+        "nome": "Golf Hotel",
+        "estrelas": 4.2,
+        "diaria": 450.90,
+        "cidade": "Rio de Janeiro",
+        "site_id": 5
+    },
+    {
+        "nome": "Xray Hotel",
+        "estrelas": 1.2,
+        "diaria": 59.70,
+        "cidade": "Belem",
+        "site_id": 6
+    },
+    {
+        "nome": 'Wabliu Hotel',
+        "estrelas": 5.0,
+        "diaria": 690.20,
+        "cidade": "Rio de Janeiro",
+        "site_id": 1
+    },
+    {
+        "nome": "Yank Hotel",
+        "estrelas": 2.7,
+        "diaria": 149.90,
+        "cidade": "São Paulo",
+        "site_id": 2
+    },
+    {
+        "nome": "Inki Hotel",
+        "estrelas": 4.2,
+        "diaria": 350.90,
+        "cidade": "Rio de Janeiro",
+        "site_id": 3
+    },
+    {
+        "nome": "Oscar Hotel",
+        "estrelas": 3.5,
+        "diaria": 279.99,
+        "cidade": "Brasilia",
+        "site_id": 5
+    },
+    {
+        "nome": "Papa Hotel",
+        "estrelas": 4.8,
+        "diaria": 430.00,
+        "cidade": "Sergipe",
+        "site_id": 6
+    },
+]

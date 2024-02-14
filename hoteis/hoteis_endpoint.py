6394ecaaf3f78163cf99005b3d5240aa882028d4
marciliodevjava <marciliodevjava@gmail.com>
2024-02-13 21:51:07 -0300

    Update hoteis_endpoint.py

--- hoteis/hoteis_endpoint.py
+++ hoteis/hoteis_endpoint.py
@@ -6,6 +6,7 @@ from diretorio.url_base import URL_HOTEIS
 from login.login_headers import headers
 
 LIST_ID_HOTEL = list()
+LIST_HOTEL_DELETE = list()
 
 requisao_get_hoteis = requests.request('GET', URL_HOTEIS, headers=headers)
 
@@ -26,6 +27,11 @@ for hotel in LIST_ID_HOTEL:
         print(hotel_json['hotel'])
 
 if len(NOMES_ID_HOTEIS) == len(DADOS_HOTEIS):
-    for hotel in NOMES_ID_HOTEIS and dados in DADOS_HOTEIS:
-        URL_GET = f'{URL_HOTEIS}/{hotel}'
-        requsicao_post= requests.request('POST', URL_GET)
+    for posicao in range(len(NOMES_ID_HOTEIS)):
+        hotel_id = NOMES_ID_HOTEIS[posicao].get('site_id')
+        URL_GET = f'{URL_HOTEIS}/{hotel_id}'
+        requsicao_post = requests.request('POST', URL_GET, json=DADOS_HOTEIS[posicao], headers=headers)
+        if requsicao_post.status_code == 201:
+            dados_cadastro_hotel = requsicao_post.json()
+            id = dados_cadastro_hotel['hotel']['hotel_id']
+            LIST_HOTEL_DELETE.append(id)
